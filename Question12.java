a24357d96056ee2795ea1f459ec2bd031146485e
YASH KUMAR <yaku15480@gmail.com>
2021-07-25 16:14:09 +0530

    BFQs

---
+++ Question12.java
@@ -0,0 +1,25 @@
+
+public class Question12 {
+
+    void show() {
+
+        System.out.println("JAVA");
+
+    }
+
+    void display() {
+        System.out.println("World");
+    }
+
+    Question12() {
+        System.out.println("Hellow");
+
+    }
+
+    public static void main(String[] args) {
+        Question12 a = new Question12();
+        a.show();
+    }
+}
+// Hellow
+// JAVA
